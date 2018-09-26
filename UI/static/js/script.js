e727899dbc3feed54e41971bfea490394e373379
nanfuka <kalungi2k6@gmail.com>
2018-09-26 19:20:09 +0300

    refactor code[#160797482]

---
+++ UI/static/js/script.js
@@ -0,0 +1,6 @@
+function picture(){ 
+    var pic = "../images/menu.jpg"
+    document.getElementById('bigpic').src = pic.replace('90x90', '225x225');
+    document.getElementById('bigpic').style.display='block';
+
+    }
